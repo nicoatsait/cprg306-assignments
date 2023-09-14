afc4b6e0b3eae4973a2b9e4ccf59e7b5b6127311
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2023-09-14 16:31:57 -0600

    fixed the course section for student info

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -6,7 +6,7 @@ const StudentInfo = () => {
   return (
     <div>
       <h1>Nicholas Gonzalez </h1>
-      <p>CPRG 306 A</p>
+      <p>CPRG 306 F</p>
       <Link href="https://github.com/nicoatsait/cprg306-assignments">
         Link to GitHub Repository
       </Link>
